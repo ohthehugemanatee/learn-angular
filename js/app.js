113c64e815e1ffd5db781bbdc7c7b5026aa3d75e
Campbell Vertesi <campbell@vertesi.com>
2015-02-20 20:24:36 +0100

    basic gemstore, first two units from codeacademy

--- js/app.js
+++ js/app.js
@@ -1,3 +1,19 @@
 /**
  * Created by ohthehugemanatee on 20/02/15.
  */
+
+(function(){
+    var app = angular.module('store', []);
+
+
+    app.controller('StoreController', function(){
+        this.product = gem ;
+    });
+
+    var gem = {
+        name: 'Dodecahedron',
+        price: 2.95,
+        description: 'Here is a description'
+    };
+
+})();
