9595ad897b205c1d26a2dd88064f19a2b8fb769b
Campbell Vertesi <campbell@vertesi.com>
2015-02-23 19:42:54 +0100

    complete level 3

--- js/app.js
+++ js/app.js
@@ -14,10 +14,18 @@
         this.tab = 1;
         this.selectTab = function(setTab) {
             this.tab = setTab;
-        }
+        };
         this.isSelected = function(checkTab) {
             return this.tab === checkTab;
-        }
+        };
+    });
+
+    app.controller("ReviewController", function() {
+        this.review = {};
+        this.addReview = function(product){
+          product.reviews.push(this.review);
+          this.review = {};
+        };
     });
 
     var gems = [
@@ -34,12 +42,23 @@
                 }
             ],
             specifications: 'No specification provided',
+            reviews: [
+                {
+                    stars: 5,
+                    body: 'I love this product!',
+                    author: 'info@example.com'
+                },
+                {
+                    stars: 2,
+                    body: 'I do not like this product!',
+                    author: 'another@example.com'
+                }]
         },
         {
             name: 'Pentagonal Gem',
             price: 5.95,
             description: 'describing a pentagonal gem',
-            canPurchase: false,
+            canPurchase: false
         }
     ];
 
