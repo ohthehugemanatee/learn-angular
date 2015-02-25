95322b5a00153348edfb8f3c56288cafe21b6cbb
Campbell Vertesi <campbell@vertesi.com>
2015-02-25 15:00:42 +0100

    level 4 complete

--- js/app.js
+++ js/app.js
@@ -10,15 +10,6 @@
         this.products = gems;
     });
 
-    app.controller("PanelController", function() {
-        this.tab = 1;
-        this.selectTab = function(setTab) {
-            this.tab = setTab;
-        };
-        this.isSelected = function(checkTab) {
-            return this.tab === checkTab;
-        };
-    });
 
     app.controller("ReviewController", function() {
         this.review = {};
@@ -27,6 +18,30 @@
           this.review = {};
         };
     });
+
+    app.directive('productTitle', function(){
+        return {
+          restrict: 'E',
+          templateUrl: 'product-title.html'
+        };
+    });
+
+    app.directive('productPanels', function(){
+        return {
+            restrict: 'E',
+            templateUrl: 'product-panels.html',
+            controller: function() {
+                this.tab = 1;
+                this.selectTab = function(setTab) {
+                    this.tab = setTab;
+                };
+                this.isSelected = function(checkTab) {
+                    return this.tab === checkTab;
+                };
+            },
+            controllerAs: 'panel'
+        };
+    })
 
     var gems = [
         {
